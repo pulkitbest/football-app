484f04d94eb3e9be208cfe5741c4e0a94f0dccac
pulkitbest <pulkitgupta38@gmail.com>
2023-07-09 16:14:05 +0530

    changed Name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <footer className="footer items-center p-3 drop-shadow-lg bg-neutral text-neutral-content mt-4">
       <div className="grid-flow-col gap-2 justify-self-end">
-        <p>Created by Palash Gupta</p>
+        <p>Created by Pulkit Gupta</p>
         <a
-          href="https://github.com/palash-07/football-app"
+          href="https://github.com/pulkitbest/football-app"
           target="_blank"
           rel="noreferrer"
         >
